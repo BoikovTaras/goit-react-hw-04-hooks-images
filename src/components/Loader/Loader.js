83f3b3bb14961390727caab2faa3b6bdd08799f3
Taras Boikov <tarasgowest@gmail.com>
2021-12-31 01:34:02 +0200

    добавляет окончательные правки хуков

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,16 +1,14 @@
 import s from './Loader.module.css';
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Loader extends Component {
-  render() {
-    return (
-      <div className={s.cssloadSquares}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    );
-  }
+export default function Loader() {
+  return (
+    <div className={s.cssloadSquares}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
 }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,7 +4,7 @@ import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default function Modal({ onClose }) {
+export default function Modal({ onClose, children }) {
   useEffect(() => {
     window.addEventListener('keydown', hendleKeyDown);
     return () => {
@@ -26,38 +26,8 @@ export default function Modal({ onClose }) {
 
   return createPortal(
     <div className={s.overlay} onClick={clickOnModal}>
-      <div className={s.modal}>{this.props.children}</div>
+      <div className={s.modal}>{children}</div>
     </div>,
     modalRoot,
   );
 }
-
-// export default class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.hendleKeyDown);
-//   }
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.hendleKeyDown);
-//   }
-
-// hendleKeyDown = e => {
-//   if (e.code === 'Escape') {
-//     this.props.onClose();
-//   }
-// };
-
-// clickOnModal = event => {
-//   if (event.target === event.currentTarget) {
-//     this.props.onClose();
-//   }
-// };
-
-//   render() {
-//     return createPortal(
-//       <div className={s.overlay} onClick={this.clickOnModal}>
-//         <div className={s.modal}>{this.props.children}</div>
-//       </div>,
-//       modalRoot,
-//     );
-//   }
-// }
